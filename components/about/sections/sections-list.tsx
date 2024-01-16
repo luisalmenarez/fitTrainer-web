b40a38b5d81091d82e54d723ce821f7e610fc529
Luis Almenarez <luisalmenarez0428@gmail.com>
2024-01-16 02:03:04 -0500

    ✨ Update animation button section about me

--- components/about/sections/sections-list.tsx
+++ components/about/sections/sections-list.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import ItemAbout from "@/components/ui/ul-item-about";
 import ButtonPrimary from "@/components/ui/button-primary";
+import { motion } from "framer-motion";
 
 const SectionList = () => {
+  const fadeInConfig = {
+    initial: "hidden",
+    whileInView: "visible",
+    viewport: { once: true, amount: 0.1 },
+    transition: { delay: 0.1, duration: 0.8 },
+    variants: {
+      hidden: { opacity: 0, y: 200 },
+      visible: { opacity: 1, y: 0 },
+    },
+  };
+
   return (
     <section className="flex flex-1 flex-col items-center gap-8">
       <ul className="flex flex-col gap-4 text-stone-100 font-semibold">
@@ -15,7 +29,9 @@ const SectionList = () => {
           <ItemAbout key={index} text={text} index={index} />
         ))}
       </ul>
-      <ButtonPrimary type="button" text="Conoce más beneficios" />
+      <motion.div {...fadeInConfig}>
+        <ButtonPrimary type="button" text="Conoce más beneficios" />
+      </motion.div>
     </section>
   );
 };
